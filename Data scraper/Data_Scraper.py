2a7f8a7e236fc2f047060615a271c3ef3a6759df
Humphrey Chama <humphreychama84@gmail.com>
2025-10-28 04:55:28 +0200

    Add files via upload

---
+++ Data scraper/Data_Scraper.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+page_to_scrape = requests.get("https://www.lusakatimes.com/")
+soup = BeautifulSoup(page_to_scrape.text,"html.parser")
+headlines = soup.findAll("h3", attrs={"class":"entry-title td-module-title"})
+#authors = soup.findAll("small", attrs={"class":"author"})
+
+
+file = open(r"C:\Users\Humphrey\Documents\SCHOOL\PERSONAL\Phython\code\csv\Headlines.csv",'w')
+csv_writer = csv.writer(file) 
+
+csv_writer.writerow("Headlines")
+
+for headline in headlines:
+    csv_writer.writerow([headline.text])
+file.close()
+
+
